43aae9b3c71fbf3ec0d59ff055bd6ea37032196a
Sajith Sampath <srs22580@gmail.com>
2023-03-28 22:13:36 +0530

    "readTeacherInfo"

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -10,11 +10,12 @@ export class AppServiceService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    if(environment.production == false){
+    this.ROOT_URL = 'http://localhost:8080'
+    /*if(environment.production == false){
       this.ROOT_URL = 'test'
     }else{
       this.ROOT_URL = 'api'
-    }
+    }*/
   }
 
   initializeDB(){
